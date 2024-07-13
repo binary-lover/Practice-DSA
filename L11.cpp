7bcbaf4dea87a809597fe101769dd5abdfa97059
binary-lover <whitedevil.hamd@gmail.com>
2024-07-13 21:06:32 +0530

    Leatcode 11: Container With Most Water

--- L11.cpp
+++ L11.cpp
@@ -14,7 +14,19 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;  // Initialize maxArea to 0 instead of -Infinity
+        while (left < right) {
+            int area = (right - left) * min(height[left], height[right]);
+            maxArea = max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }}
+        return maxArea;
+
     }
 };
 
@@ -22,6 +34,6 @@ int main(){
     Solution s;
     vector<int> height = {1,8,6,2,5,4,8,3,7};
     cout << s.maxArea(height) << endl;
-    
+
     return 0;
 }
