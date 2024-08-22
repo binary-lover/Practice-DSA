363a8b8131e36db8b4ebe3f2ca711ef98697ff13
binary-lover <whitedevil.hamd@gmail.com>
2024-08-22 18:01:29 +0530

    binary search code

---
+++ binarySearch.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+    
+public:
+    int binarySearch(vector<int>& arr,int k){
+        int left = 0;
+        int right = arr.size() -1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(k==arr[mid]){
+                return mid;
+            }
+            else if (k > arr[mid]){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+
+        return -1;
+    }
+
+};
+
+
+
+int main(){
+    std::vector<int> array = {1,2,3,4,5,6};
+    Solution obj;
+    int k;
+    cout<<"Enter num for binary searching: ";
+    cin>>k;
+    int result = obj.binarySearch(array,k);
+    if(k != -1){
+        cout<<"Element found at index: "<<result<<endl;
+    }
+    else {
+        cout<<"not found..!"<<endl;
+    }
+    
+    return 0;
+}
