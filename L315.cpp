1352e15aab53759937b7a0a32dfe5bf2b921324e
binary-lover <whitedevil.hamd@gmail.com>
2024-08-23 19:23:17 +0530

    solution of Leetcode 315. Count of Smaller Numbers After Self

--- L315.cpp
+++ L315.cpp
@@ -8,48 +8,49 @@ using namespace std;
 
 class Solution {
 public:
-    int binarySearch(vector<int>& arr,int k){
-        int left = 0;
-        int right = arr.size() -1;
-
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(k==arr[mid]){
-                return mid;
-            }
-            else if (k > arr[mid]){
-                left = mid + 1;
-            }
-            else{
-                right = mid - 1;
-            }
+ int findInsertPosition(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size();
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
         }
-
-        return -1;
     }
-    vector<int> countSmaller(vector<int>& nums) {
-        vector<int> temp = nums;
-        sort(temp.begin(),temp.end());
-        vector<int> result;
-        for ( int i = 0; i < nums.size();i++){
-            int target = nums[i];
-            int newIndex = binarySearch(temp,target);
+    return left;
+} // this function is used to find the position where the element should be inserted in the array
 
-            if(newIndex>i){
-                int add = newIndex - i;
-                result[i] = add;
-            }
-        }
-        return result;
+vector<int> countSmaller(vector<int>& nums) {
+    vector<int> temp;
+    vector<int> result(nums.size());
+
+    for (int i = nums.size() - 1; i >= 0; --i) {
+        int pos = findInsertPosition(temp, nums[i]);
+        result[i] = pos;
+        temp.insert(temp.begin() + pos, nums[i]); //
     }
+
+    return result;
+}
+
 };
 int main(){
     Solution s;
-    vector<int> nums = {5,2,6,1};
+    vector<int> nums = {5,4,3,2,1}; // {1,2,5,6} 
+    // {12, 1, 3, 2, 0, 11, 4}; sort = {0, 1, 2, 3, 4, 11, 12}
     vector<int> res = s.countSmaller(nums);
     for(int i=0; i<res.size(); i++){
         cout<<res[i]<<" ";
     }
+
+    // vector<int> nums1 = {12, 1, 3, 2, 0, 11, 4};
+    // // {12, 1, 3, 2, 0, 11, 4}; sort = {0, 1, 2, 3, 4, 11, 12}
+    // vector<int> res1 = s.countSmaller(nums1);
+    // for(int i=0; i<res1.size(); i++){
+    //     cout<<res1[i]<<" ";
+    // }
 
 
     return 0;
